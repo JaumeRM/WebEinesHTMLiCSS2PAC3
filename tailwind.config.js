1a7600fd31dab9b950a50a7251bb3563730a8f53
Jaume Ramon <jaume_ramon@andorra.ad>
2025-01-09 12:56:18 +0100

    Tailwind proves

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pac2-font-color": "rgb(255, 255, 255)",
+        "pac2-font-color-dark": "rgb(0, 0, 0)",
+        "pac2-font-title-color": "rgb(255, 255, 255)",
+        "pac2-color-background": "rgb(255, 255, 255)",
+        "pac2-color-background-dark": "rgb(0, 0, 0)",
+        "pac2-color-ciber": "rgb(22, 74, 124)",
+        "pac2-color-background-header": "rgb(22, 74, 124)",
+        "pac2-color-background-footer": "rgb(22, 74, 124)",
+      },
+      fontSize: {
+        gran: "50px",
+      },
+      fontFamily: {
+        "pac2-font": ["Montserrat"],
+        "pac2-font-header": ["Lato"],
+        "pac2-font-title": ["Lato"],
+        "pac2-font-foto-title": ["Lato"],
+      },
+    },
   },
   plugins: [],
 };
